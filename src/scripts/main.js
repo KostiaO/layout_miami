b60dd3ef20c48830622067cb306d45e0b29b53cf
unknown <avicostov2@gmail.com>
2022-03-29 13:31:02 +0200

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#bar') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
-});
